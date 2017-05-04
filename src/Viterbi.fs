f642aa61706778c346952bb13268c5b0583dc376
Michał Niegrzybowski <michal.niegrzybowski@gmail.com>
2017-05-04 18:06:21 +0200

    Viterbi done :)

--- src/Viterbi.fs
+++ src/Viterbi.fs
@@ -1,5 +1,6 @@
 namespace markov.core
     open System
+    open System.Collections.Generic
 
     module Viterbi =
         let private getMaxes(T1: double[,], i: int, selector) =
@@ -15,13 +16,14 @@ namespace markov.core
             let selector = fun index elem -> (index, elem)
             getMaxes(T1, n, selector) |> fst
 
-        let Calculate(space: int[], states: int[], probabilities: double[], observations: int[], A: double[][], B: double[][]) =
+        let Calculate(states: int[], probabilities: double[], observations: int[], A: double[][], B: double[][]) =
             let T1: double[,] = Array2D.create states.Length observations.Length 0.0
             let T2: int[,] = Array2D.create states.Length observations.Length 0
             let z = Array.zeroCreate observations.Length
             let x = Array.zeroCreate observations.Length
+
             for i in [0..states.Length - 1] do
-                T1.[i, 1] <- probabilities.[i] * B.[0].[0]
+                T1.[i, 0] <- probabilities.[i] * B.[i].[observations.[0]]
 
             for i in [1..observations.Length - 1] do
                 for j in [0..states.Length - 1] do
@@ -32,7 +34,12 @@ namespace markov.core
             x.[observations.Length - 1] <- states.[z.[observations.Length - 1]]
             for i = observations.Length - 1 downto 1 do
                 z.[i-1] <- T2.[z.[i], i]
-                x.[i - 1] <- space.[z.[i-1]]
+                x.[i - 1] <- observations.[z.[i-1]]
             x
 
+        let Predict(states: int[], probabilities: double[], observations: int[], A: double[][], B: double[][], mapping: IDictionary<int, string>) =
+            (states, probabilities, observations, A, B)
+            |> Calculate
+            |> Array.map (fun x -> mapping.[x])
+            |> String.concat ""
             
--- test/ViterbiTests.fs
+++ test/ViterbiTests.fs
@@ -3,17 +3,53 @@ module Tests.Viterbi
 open Expecto
 open markov.core
 
-[<Tests>]
-let baseTests =
-  let O = [|0;1;2|]
+module Lecture = 
   let S = [|0;1|]
   let pi = [|0.6;0.4|]
   let Y = [|0;1;0;2|]
   let A = [|[|0.7; 0.3|];[|0.4; 0.6|]|]
   let B = [|[|0.1;0.4;0.5|]; [|0.7; 0.2; 0.1|]|]
-  let result = (O, S, pi, Y, A, B) |> Viterbi.Calculate
+  let mapping = dict [(0, "H"); (1, "C")]
+  [<Tests>]
+  let calculation =
+    let result = (S, pi, Y, A, B) |> Viterbi.Calculate
+    
+    testList "Forward Backward from Lecture" [
+      testCase "for calculation" <| fun _ ->
+        Expect.sequenceEqual result [|1;1;1;0|] "should equal to sequence 1 1 1 0"
+    ]
 
-  testList "Forward Backward from Lecture" [
-    testCase "for first character" <| fun _ ->
-      System.Console.WriteLine(sprintf "%A" result)
-  ]
+  [<Tests>]
+  let prediction =
+    let result = (S, pi, Y, A, B, mapping) |> Viterbi.Predict
+    
+    testList "Forward Backward from Lecture" [
+      testCase "for prediction" <| fun _ ->
+        Expect.equal result "CCCH" "should equal to CCCH"
+    ]
+
+module Lechia = 
+  let S = [|0;1;2|]
+  let A = [| [|0.35; 0.3; 0.35|]; [|0.2; 0.2; 0.6|]; [|0.36; 0.23; 0.41|]|]
+  let B = [| [|0.62; 0.38|]; [|0.625; 0.375|]; [|0.37; 0.63|]|]
+  let pi = [|0.57;0.29;0.17|]
+  let Y = [|0; 1; 0; 0; 1; 0; 1|]
+  let mapping = dict [(0, "W"); (1, "D"); (2, "L")]
+
+  [<Tests>]
+  let calculation =
+    let result = (S, pi, Y, A, B) |> Viterbi.Calculate
+    
+    testList "Forward Backward from exercise" [
+      testCase "for calculation" <| fun _ ->
+        Expect.sequenceEqual result [|0;0;0;1;0;1;2|] "should equal to sequence 1 1 1 0"
+    ]
+
+  [<Tests>]
+  let prediction =
+    let result = (S, pi, Y, A, B, mapping) |> Viterbi.Predict
+    
+    testList "Forward Backward from exercise" [
+      testCase "for prediction" <| fun _ ->
+        Expect.equal result "WWWDWDL" "should equal to WWWWWWW"
+    ]
